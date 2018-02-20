7f2bb5a4c077a969100ba07ec3f8845a1e739dd4
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2018-02-19 20:05:01 -0500

    fichier de script du chapitre 5

--- buhlmann-straub.R
+++ buhlmann-straub.R
@@ -14,22 +14,59 @@
 ## Charger le package actuar dans la session de travail.
 library(actuar)
 
-data(hachemeister)
+## Les données de Hachemeister (1975) sont fournies avec le
+## paquetage actuar sous forme d'une matrice de 5 lignes et de
+## 25 colonnes. La première colonne contient un identifiant
+## d'État, les colonnes 2-13 les montants de sinistres moyens
+## et les colonnes 14-25, les nombres de sinistres.
 hachemeister
 
-## Modèle de Bühlmann (sans tenir compte des volumes)
+## Dès lors qu'un jeu de données comporte des colonnes autres
+## que l'identifiant du contrat et les données de sinistres,
+## il faut avoir recours aux arguments suivants de 'cm'.
+##
+## ratios: expression indiquant les colonnes dans lesquelles
+##  se trouvent les données de sinistres;
+## weights: expression indiquant les colonnes dans lesquelles
+##  se trouvent les poids associés aux données de sinistres.
+##
+## Ces deux arguments sont utilisés comme l'argument 'select'
+## de la fonction 'subset'. Cela permet d'indicer de manière
+## très intuitive les colonnes du jeu de données.
+##
+## Par exemple, l'expression ci-dessous permet d'extraire les
+## ratios des cinq premières années des données de
+## Hachemeister.
+subset(hachemeister, select = ratio.1:ratio.5)
+
+## Ajustement du modèle de Bühlmann aux données de
+## Hachemeister (sans tenir compte des volumes).
 fit.B <- cm(~state, hachemeister,
             ratios = ratio.1:ratio.12)
 summary(fit.B)
 
-## Modèle de Buhlmann-Straub (estimateur sans biais de la variance
-## inter).
+## L'argument 'method' de 'cm' permet de choisir l'estimateur
+## de la variance inter. Trois choix sont disponibles.
+##
+## "Buhlmann-Gisler": notre estimateur sans biais;
+## "Ohlsson": équivalent à "Buhlmann-Gisler" dans le modèle de
+##   Buhlmann-Straub;
+## "iterative": pseudo-estimateur de Bichsel-Straub.
+##
+## La vignette "credibility" du paquetage contient tous les
+## détails sur les méthodes d'estimation de la variance inter.
+vignette("credibility")
+
+## Ajustement du modèle de Bühlmann-Straub avec l'estimateur
+## sans biais de la variance inter (la méthode par défaut,
+## donc non spécifiée ici). Il faut spécifier des poids.
 fit.BS1 <- cm(~state, hachemeister,
              ratios = ratio.1:ratio.12,
              weights = weight.1:weight.12)
 summary(fit.BS1)
 
-## Modèle de Buhlmann-Straub (pseudo-estimateur de la variance inter).
+## Même chose, mais avec le pseudo-estimateur de la variance
+## inter.
 fit.BS2 <- cm(~state, hachemeister,
              ratios = ratio.1:ratio.12,
              weights = weight.1:weight.12,
